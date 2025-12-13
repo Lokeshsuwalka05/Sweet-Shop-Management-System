17f2c15135b5e077b5093cfe629d34dd414335f5
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-12-13 19:01:00 +0530

    chore: initial backend setup

---
+++ Backend/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const { connectDB } = require("./config/Database");
+const { authRouter } = require("./routes/auth");
+require("dotenv").config();
+const app = express();
+// Use conventional uppercase PORT, fallback to 3000
+const port = Number(process.env.port) || 3000;
+app.use(express.json());
+app.use("/", authRouter);
+connectDB()
+  .then(() => {
+    console.log("Database connection established....");
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}`);
+    });
+  })
+  .catch((e) => console.log(e));
---
+++ Backend/src/config/Database.js
@@ -0,0 +1,6 @@
+const mongoose = require("mongoose");
+const connectDB = async () => {
+  const uri = process.env.MONGODB_URI;
+  await mongoose.connect(uri);
+};
+module.exports = { connectDB };
