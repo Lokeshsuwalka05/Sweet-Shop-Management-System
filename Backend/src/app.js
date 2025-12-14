0f331fdba85efcce816a2f01ed81390af2cb10cc
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-12-14 23:07:12 +0530

    Resolve cors issues

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,9 +3,17 @@ const { authRouter } = require("./routes/auth");
 const { sweetsRouter } = require("./routes/sweets");
 const { inventoryRouter } = require("./routes/inventory");
 const cookieParser = require("cookie-parser");
+const cors = require("cors");
 const app = express();
 app.use(express.json());
 app.use(cookieParser());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "PUT", "DELETE", "OPTIONS"],
+  })
+);
 app.use("/", authRouter);
 app.use("/", sweetsRouter);
 app.use("/", inventoryRouter);
