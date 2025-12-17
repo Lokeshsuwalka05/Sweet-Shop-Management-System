2d8dffcb706dabeb87efa5e05969bc52b90b904f
Lokeshsuwalka05 <lokeshsuwalka2002@gmail.com>
2025-12-17 16:12:32 +0530

    BUG:fix bg for header in dark mode

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -16,16 +16,16 @@ function Header() {
   };
   const { user, logout, isAdmin } = useAuth();
   return (
-    <div>
-      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4 ">
+    <div className="bg-base-100 border-b shadow-sm">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-4">
         <div className="flex items-center justify-between">
           <div className="flex items-center gap-2">
             <img src={logo} alt="Sweet Shop Logo" className="h-8 w-8" />
-            <h1 className="text-2xl font-bold text-gray-900">Sweet Shop</h1>
+            <h1 className="text-2xl font-bold text-base-content">Sweet Shop</h1>
           </div>
           <div className="flex items-center gap-4">
             {user && (
-              <span className="text-sm text-gray-600">
+              <span className="text-sm text-base-content/70">
                 Welcome, {user?.firstName}!
               </span>
             )}
